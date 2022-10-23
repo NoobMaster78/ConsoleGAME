633603a858daf3bca181ee9068dd80c7a5da2ce6
Kayle <Skiletoons@mail.ru>
2022-10-23 18:53:03 +0700

    ConsoleGame 1var

---
+++ ConsoleGame/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGame
+{
+    class Character
+    {
+       private int health = 100;
+       private int x = 50;
+        private int y = 25;
+        public int Health
+        {
+            get => health;
+            set => health = value;
+        }
+        public int X => x;
+        public int Y => y;
+        public string Coordinates => $"[{x},{y}]";
+        public bool IsAlive => health > 0;
+
+
+    }
+}
---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character hero = new Character();
+            Console.WriteLine($"Hero's health: {hero.Health} | Hero is alive: {hero.IsAlive}");
+            hero.Health = 0;
+            Console.WriteLine($"Hero's health: {hero.Health} | Hero is alive: {hero.IsAlive}");
+            Console.WriteLine(hero.Coordinates);
+        }
+    }
+}
